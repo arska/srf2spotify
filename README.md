51081145e2843c89f4562e43a1a0538ede0561b1
Aarno Aukia <aarno@aukia.com>
2015-11-01 00:29:28 +0100

    clean up

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-SRF2Spotify
+# SRF2Spotify
 
 Swiss radio SRF has a nicely compiled weekly "rock special" programme that I have been listening to as podcast. I'd like to create a playlist and shuffle though the songs in spotify.
+
+## usage
+1. register yourself at https://developer.spotify.com/my-applications, enter "http://localhost/" for the "Redirect URIs" (it is fine if you don't have a webserver running)
+2. create a ".env" file with the following contents:
+> SPOTIPY_CLIENT_ID='<Client ID from above>'
+> SPOTIPY_CLIENT_SECRET='<Client Secret from above>'
+> SPOTIPY_REDIRECT_URI='http://localhost/'
+3. run the script with =env $(cat .env | xargs) python srf2spotify.py <spotifyusername> <podcast feed url>= (see more options with -h)
+4. if you are missing some libraries add them with =sudo -H pip install <modulename>=
+4. the script will ask you to authorize the client_id for your spotifyusername by visiting a spotify URL that will redirect to the URI above with the authorization code, e.g. http://localhost/?code=..., that you need to copy/paste to the script.
+5. the authorization will be saved in ".cache-<spotifyusername>"
+6. 
--- srf2spotify.py
+++ srf2spotify.py
@@ -15,51 +15,99 @@ import json
 import spotipy
 import spotipy.util as util
 import logging
+import argparse
 
 def main(argv=None):
-  if argv is None:
-    argv = sys.argv
-  feed = "http://podcasts.srf.ch/rock_special_mpx.xml"
-  spotifyusername = "aarnoaukia"
-  spotifyauthscope = 'playlist-modify-public playlist-modify-private'
-  playlistname = "SRF Rock Special"
-  logging.basicConfig(level=logging.DEBUG)
+  parser = argparse.ArgumentParser(description='Sync feed with spotify playlist')
+  parser.add_argument('username',help='spotify username/account to sync the playlist to')
+  parser.add_argument('feed',help='podcast feed URL, e.g. http://podcasts.srf.ch/rock_special_mpx.xml')
+  parser.add_argument('--id',required=False,default=None,help='spotify playlist ID to sync to. overrides the name below.')
+  parser.add_argument('--name',required=False,default=None,help='spotify playlist name to sync to. playlist is created if there is no such playlist. the podcast title is used if omitted.')
+  parser.add_argument('-v','--verbose',help='output debug logging',action='store_true')
+  args = parser.parse_args()
 
+  if args.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+
+  logging.debug("got args: %s" % args)
+
+  token = spotipy.util.prompt_for_user_token(args.username,'playlist-modify-public')
+  logging.debug("go auth token %s" % token)
+  spotify = spotipy.Spotify(auth=token)
+  logging.debug("logged in to spotify")
+
+  sync_feed_with_playlist(feed=args.feed,spotify=spotify,spotifyusername=args.username,playlist_name=args.name,playlist_id=args.id)
+
+
+def sync_feed_with_playlist(feed,spotifyusername,spotify,playlist_name=None,playlist_id=None):
+  """ Sync a SRF3 podcast feed with a spotify playlist
+
+      Parameters:
+        - feed - podcast feed URL, e.g. http://podcasts.srf.ch/rock_special_mpx.xml
+        - spotifyusername - your spotify username/account to sync the playlist to
+        - spotify - authenticated spotipy object, e.g. spotipy.Spotify(auth=spotipy.util.prompt_for_user_token(spotifyusername,'playlist-modify-public'))
+        - playlist_id - spotify playlist to sync the feed to
+        - playlist_name - name for the spotify playlist to create/update, if both id and name is empty the RSS feed title is used
+  """
+  # first get the start/end dates for each episode from the podcast feed
   dates = get_datetime_from_podcast(feed)
   logging.info("got %d datetimes" % len(dates))
   logging.debug(dates)
 
+  # then query the songlog for artist/title during the episode intervals
   songs = []
   for (start,end) in dates:
     songs += get_srf3_songlog(start,end)
   logging.info("got %d songs" % len(songs))
   logging.debug(songs)
 
-  token = spotipy.util.prompt_for_user_token(spotifyusername,spotifyauthscope)
-  logging.debug("go auth token %s" % token)
-  spotify = spotipy.Spotify(auth=token)
-  logging.debug("logged in to spotify")
+  if len(songs) == 0:
+    # bail out if the list is empty before removing everything
+    sys.exit(1)
 
+  # query spotify for the artist/titles and get the track IDs
   songs = spotify_search_songs(songs,spotify)
   logging.info("got %d unique songs from spotify" % len(songs))
   logging.debug(songs)
 
-  playlists = spotify.user_playlists(spotifyusername)
-  logging.info("Got %d existing playlists" % len(playlists))
-  logging.debug(playlists)
-  targetplaylist = ""
-  for playlist in playlists['items']:
-    if playlist['name'] == playlistname:
-      targetplaylist = playlist['uri']
-      logging.info("found existing playlist %s %s" % (playlistname,targetplaylist))
-      break
-  if targetplaylist == "":
-    # the playlist does not exist yet
-    playlist = spotify.user_playlist_create(spotifyusername,playlistname)
-    targetplaylist = playlist['uri']
-    logging.info("created new playlist %s %s" % (targetplaylist,playlistname))
+  if len(songs) == 0:
+    # bail out if the list is empty before removing everything
+    sys.exit(1)
 
+  # use the playlist_id if there is one, else use the playlist named like playlist_name or create one like the podcast title
+  if not playlist_id:
+    if not playlist_name:
+      # use the podcast title if nothing specified
+      data = get_podcast_data(feed)
+      playlist_name = data['rss']['channel']['title']
+
+    # since one can't query for a specific playlist by name we have to get them all and search ourselves
+    playlists = spotify.user_playlists(spotifyusername)
+    logging.info("Got %d existing playlists for user %s" % (len(playlists),spotifyusername))
+    logging.debug(playlists)
+
+    targetplaylist = ""
+    for playlist in playlists['items']:
+      if playlist['name'] == playlist_name:
+        targetplaylist = playlist['uri']
+        logging.info("found existing playlist %s %s" % (playlist_name,targetplaylist))
+        break
+    if targetplaylist == "":
+      # the playlist does not exist yet
+      playlist = spotify.user_playlist_create(spotifyusername,playlist_name)
+      targetplaylist = playlist['uri']
+      logging.info("created new playlist %s %s" % (targetplaylist,playlist_name))
+  else:
+    logging.debug("setting playlist id to %s" % playlist_id)
+    targetplaylist = playlist_id
+
+  # there is a spotify.user_playlist_replace_tracks() but it accepts only 100 tracks at a time
+  # the rest has to be spotify.user_playlist_add_tracks() manually
+  # so we're adding/deleting them ourselves
+
+  # get all old tracks from the playlist to later delete the superfluous ones
   playlisttracks = []
+  logging.debug("spotify_get_all_trackids %s %s %s" % (spotifyusername,targetplaylist,spotify))
   for track in spotify_get_all_trackids(spotifyusername,targetplaylist,spotify):
     if track not in songs:
       logging.info("removing %s from playlist" % track['uri'])
@@ -67,12 +115,20 @@ def main(argv=None):
     else:
       playlisttracks.append(track)
 
+  # add podcast songs not already in the playlist
   for song in songs:
     if song not in playlisttracks:
       logging.info("adding %s to playlist" % song)
       spotify.user_playlist_add_tracks(spotifyusername,targetplaylist,[song])
 
 def spotify_get_all_trackids(spotifyusername,targetplaylist,spotify):
+  """ Get all tracks for a playlist - the API only provides 100 tracks at the time
+
+      Parameters:
+        - spotifyusername - spotify account to look for the playlist
+        - targetplaylist - playlist ID
+        - spotify - authenticated spotipy object
+  """
   logging.info("getting full list of tracks for %s" % targetplaylist)
   playlist = spotify.user_playlist(spotifyusername,targetplaylist)
   result = []
@@ -84,6 +140,12 @@ def spotify_get_all_trackids(spotifyusername,targetplaylist,spotify):
   return result
 
 def spotify_search_songs(songs,spotify=spotipy.Spotify()):
+  """ search spotify for artist/title and return the unique spotify track IDs
+
+      Parameters:
+        - songs - iterable of mapping (e.g. array of hash) with keys 'artist' and 'title'
+        - spotify - spotipy object, doesn't need to be authenticated for search (although there is a higher rate limit for authenticated users)
+  """
   results = []
   for song in songs:
     result = spotify.search("artist:"+song['artist']+" track:"+song['title'],limit=1,type='track')
@@ -91,12 +153,14 @@ def spotify_search_songs(songs,spotify=spotipy.Spotify()):
       track = result['tracks']['items'][0]['uri']
       if track not in results:
         results.append(track)
+    else:
+      logging.info("no spotify track found for %s" % song)
   return results
 
-def get_srf3_songlog(start,end):
+def get_srf3_songlog(start,end,datapollingurl="http://ws.srf.ch/songlog/log/channel/",datachannelid="dd0fa1ba-4ff6-4e1a-ab74-d7e49057d96f"):
+  """ Get the song log for SRF3 radio by start/end datetime. Returns an array of dict with 'artist' and 'title' keys"""
+  # example lookup from http://www.srf.ch/sendungen/rock-special/the-shit-kalifornien-bern-einfach for SRF3
   # http://ws.srf.ch/songlog/log/channel/dd0fa1ba-4ff6-4e1a-ab74-d7e49057d96f.json?callback=songLogPollerCallback_episode_fullview&fromDate=2015-10-28T20%3A03%3A00&toDate=2015-10-28T22%3A00%3A00&page.size=100&page.page=0&page.sort=playedDate&page.sort.dir=desc
-  datapollingurl="http://ws.srf.ch/songlog/log/channel/"
-  datachannelid="dd0fa1ba-4ff6-4e1a-ab74-d7e49057d96f"
   params = {
     'fromDate': start.strftime("%Y-%m-%dT%H:%M:%S"),
     'toDate': end.strftime("%Y-%m-%dT%H:%M:%S")
@@ -113,10 +177,7 @@ def get_srf3_songlog(start,end):
 
 def get_datetime_from_podcast(feed):
   """return the (start,end) datetime for episodes from the podcast feed pubDate (=episode ending time) and rounded duration"""
-  filehandle = urllib2.urlopen(feed)
-  data = filehandle.read()
-  filehandle.close()
-  data = xmltodict.parse(data)
+  data = get_podcast_data(feed)
   dates = []
   for episode in data['rss']['channel']['item']:
     # pubDate: Wed, 28 Oct 2015 22:00:00 +0100
@@ -126,6 +187,13 @@ def get_datetime_from_podcast(feed):
     dates.append((start,end))
   return dates
 
+def get_podcast_data(feed):
+  """Load Podcast from feed URL and parse XML into dict"""
+  filehandle = urllib2.urlopen(feed)
+  data = filehandle.read()
+  filehandle.close()
+  return xmltodict.parse(data)
+
 if __name__ == "__main__":
   """Boilerplate main function call"""
-  sys.exit(main())
+  sys.exit(main(sys.argv))
