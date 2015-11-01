cbb73f7ff3b33845adeb9ea65ef3ab93d2c328c0
Aarno Aukia <aarno@aukia.com>
2015-11-01 02:19:23 +0100

    add cron.sh with spotify urls

---
+++ cron.sh
@@ -0,0 +1,11 @@
+env $(cat .env | xargs) python srf2spotify.py aarnoaukia http://podcasts.srf.ch/black-music_special_mpx.xml
+# https://open.spotify.com/user/aarnoaukia/playlist/6Mcg2gNk9b9u9g2HCmUcy5
+
+env $(cat .env | xargs) python srf2spotify.py aarnoaukia http://podcasts.srf.ch/pop_routes_mpx.xml
+# https://open.spotify.com/user/aarnoaukia/playlist/4sq8otplfToytYMevleTBi
+
+env $(cat .env | xargs) python srf2spotify.py aarnoaukia http://podcasts.srf.ch/sounds_mpx.xml
+# https://open.spotify.com/user/aarnoaukia/playlist/5KfO8EsKEX0T0mjbJmHaMD
+
+env $(cat .env | xargs) python srf2spotify.py aarnoaukia http://podcasts.srf.ch/rock_special_mpx.xml
+# https://open.spotify.com/user/aarnoaukia/playlist/4Uxp8OaU8Fc06cjpQgZxdt
