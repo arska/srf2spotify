0dcff9dc3ba81cbf225ce5b4e44779aa333d823b
Aarno Aukia <aarno@aukia.com>
2015-11-17 00:17:27 +0100

    add to playlist only

--- cron.sh
+++ cron.sh
@@ -4,26 +4,26 @@
 # set the spotify-credentials from the environment variable, see README
 echo $SPOTIPY_CACHE > .cache-$SPOTIFY_USERNAME
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/black-music_special_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/black-music_special_mpx.xml
 # https://open.spotify.com/user/$SPOTIFY_USERNAME/playlist/6Mcg2gNk9b9u9g2HCmUcy5
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/pop_routes_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/pop_routes_mpx.xml
 # https://open.spotify.com/user/$SPOTIFY_USERNAME/playlist/4sq8otplfToytYMevleTBi
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/sounds_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/sounds_mpx.xml
 # https://open.spotify.com/user/$SPOTIFY_USERNAME/playlist/5KfO8EsKEX0T0mjbJmHaMD
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/rock_special_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/rock_special_mpx.xml
 # https://open.spotify.com/user/$SPOTIFY_USERNAME/playlist/4Uxp8OaU8Fc06cjpQgZxdt
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/ch_special_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/ch_special_mpx.xml
 # https://open.spotify.com/user/aarnoaukia/playlist/72i3UMC1LDoi5auo9AACvH
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/hitparade_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/hitparade_mpx.xml
 # https://open.spotify.com/user/aarnoaukia/playlist/1UH2A80CsXn0Cp5njr8x0E
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/reggae_special_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/reggae_special_mpx.xml
 # https://open.spotify.com/user/aarnoaukia/playlist/0mdijROnuU2CRY7MKcDv35
 
-python srf2spotify.py -v $SPOTIFY_USERNAME http://podcasts.srf.ch/world_music_special_mpx.xml
+python srf2spotify.py -av $SPOTIFY_USERNAME http://podcasts.srf.ch/world_music_special_mpx.xml
 # https://open.spotify.com/user/aarnoaukia/playlist/3K9MLAIkBey8i1GSCQ95NF
