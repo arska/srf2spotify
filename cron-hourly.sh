04d336dca30b52876d92c62b2c3bbf7db90a3905
Aarno Aukia <aarno@aukia.com>
2015-12-02 10:27:16 +0200

    remove verbose from cron

--- cron-hourly.sh
+++ cron-hourly.sh
@@ -4,20 +4,20 @@
 # set the spotify-credentials from the environment variable, see README
 echo $SPOTIPY_CACHE > .cache-$SPOTIFY_USERNAME
 
-python jouluradio2spotify.py -av --id 3W2gEbnEqytlM221BSusCb $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/jouluradiolast20.json
+python jouluradio2spotify.py -a --id 3W2gEbnEqytlM221BSusCb $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/jouluradiolast20.json
 # https://open.spotify.com/user/aarnoaukia/playlist/3W2gEbnEqytlM221BSusCb
 
-python jouluradio2spotify.py -av --id 4KXmZgDJG5J4MMDIM5wSAt $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/jouradiolast20.json
+python jouluradio2spotify.py -a --id 4KXmZgDJG5J4MMDIM5wSAt $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/jouradiolast20.json
 # https://open.spotify.com/user/aarnoaukia/playlist/4KXmZgDJG5J4MMDIM5wSAt
 
-python jouluradio2spotify.py -av --id 3HY2gCw7bfyxK4TtbeHb4s $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/Rouhea_Joululast20.json
+python jouluradio2spotify.py -a --id 3HY2gCw7bfyxK4TtbeHb4s $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/Rouhea_Joululast20.json
 # https://open.spotify.com/user/aarnoaukia/playlist/3HY2gCw7bfyxK4TtbeHb4s
 
-python jouluradio2spotify.py -av --id 75oYEbhELvE36eIsT4sDzJ $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/Kauneimmatlast20.json
+python jouluradio2spotify.py -a --id 75oYEbhELvE36eIsT4sDzJ $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/Kauneimmatlast20.json
 # https://open.spotify.com/user/aarnoaukia/playlist/75oYEbhELvE36eIsT4sDzJ
 
-python jouluradio2spotify.py -av --id 5JXXR8XDKMWrZvdsbmbnLO $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/pikkulast20.json
+python jouluradio2spotify.py -a --id 5JXXR8XDKMWrZvdsbmbnLO $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/pikkulast20.json
 # https://open.spotify.com/user/aarnoaukia/playlist/5JXXR8XDKMWrZvdsbmbnLO
 
-python jouluradio2spotify.py -av --id 11GKkK9iKKWP4Y0zV9teo7 $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/klassinenjoululast20.json
+python jouluradio2spotify.py -a --id 11GKkK9iKKWP4Y0zV9teo7 $SPOTIFY_USERNAME http://www.jouluradio.fi/biisilista/klassinenjoululast20.json
 # https://open.spotify.com/user/aarnoaukia/playlist/11GKkK9iKKWP4Y0zV9teo7
